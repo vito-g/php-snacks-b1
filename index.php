84650594543be15e15dba82f9558f78b6b88fa3e
Vito Gentile <gentilevito1076@gmail.com>
2021-02-26 16:27:16 +0100

    Creazione Array Single matches

--- index.php
+++ index.php
@@ -8,8 +8,41 @@
 
     <?php
 
+      $matchOne = [
+        'homeTeam' => 'De\'Longhi Treviso',
+        'awayTeam' => 'Dolomiti Energia Trentino',
+        'homeTeamScore' => 16,
+        'awayTeamScore' => 12
+      ];
 
-     ?>
+      $matchTwo = [
+        'homeTeam' => 'Virtus Roma',
+        'awayTeam' => 'Fortitudo Lavoropiù Bologna',
+        'homeTeamScore' => 0,
+        'awayTeamScore' => 14
+      ];
+
+      $matchThree = [
+        'homeTeam' => 'Openjobmetis Varese',
+        'awayTeam' => 'Germani Brescia',
+        'homeTeamScore' => 10,
+        'awayTeamScore' => 16
+      ];
+
+      $matchFour = [
+        'homeTeam' => 'UNAHOTELS Reggio Emilia',
+        'awayTeam' => 'A|X Armani Exchange Milano',
+        'homeTeamScore' => 14,
+        'awayTeamScore' => 30
+      ];
+
+      var_dump($matchOne);
+
+
+
+
+
+    ?>
 
   </body>
 </html>
