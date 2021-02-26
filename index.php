b242eb7f2a124897ad1c5e3206f5e4bdb85eef64
Vito Gentile <gentilevito1076@gmail.com>
2021-02-26 17:37:34 +0100

    SNACK 1 OK: Stampa a Schermo, Stretto Necessario

--- index.php
+++ index.php
@@ -37,26 +37,26 @@
         'awayTeamScore' => 30
       ];
 
-      var_dump($matchOne);
-      echo '<br>' . '<br>';
+      // var_dump($matchOne);
+      // echo '<br>' . '<br>';
       $allMatchesDaily = [];
 
       array_push($allMatchesDaily, $matchOne, $matchTwo, $matchThree, $matchFour );
-      echo  '<strong>' . 'L\'Array di tutti i match di giornata:' . '</strong>' . '<br>';
-      var_dump($allMatchesDaily);
-      echo '<br>' . '<br>';
+      // echo  '<strong>' . 'L\'Array di tutti i match di giornata:' . '</strong>' . '<br>';
+      // var_dump($allMatchesDaily);
+      // echo '<br>' . '<br>';
       //Funziona:
-      echo 'Single Match' . '<br>';
-      echo $matchOne['homeTeam'] . '-' . $matchOne['awayTeam'] . ' ' . '|' . ' ' . $matchOne['homeTeamScore'] . '-' . $matchOne['awayTeamScore'];
+      // echo 'Single Match' . '<br>';
+      // echo $matchOne['homeTeam'] . '-' . $matchOne['awayTeam'] . ' ' . '|' . ' ' . $matchOne['homeTeamScore'] . '-' . $matchOne['awayTeamScore'];
 
-      echo '<br>' . '<br>';
+      // echo '<br>' . '<br>';
 
       // for ($i = 0; $i < count($allMatchesDaily); $i++) {
       //   echo $matchOne['homeTeam'] . '-' . $matchOne['awayTeam'] . ' ' . '|' . ' ' . $matchOne['homeTeamScore'] . '-' . $matchOne['awayTeamScore'];
       // }
 
-      echo '<br>' . '<br>';
-      echo 'Match di giornata' . '<br>';
+      // echo '<br>' . '<br>';
+      echo '<strong>' . 'Match di giornata' . '</strong>' .'<br>';
       for ($i = 0; $i < count($allMatchesDaily); $i++) {
         echo $allMatchesDaily[$i]['homeTeam'] . '-' . $allMatchesDaily[$i]['awayTeam'] . ' ' . '|' . ' ' . $allMatchesDaily[$i]['homeTeamScore'] . '-' . $allMatchesDaily[$i]['awayTeamScore'] . '<br>';
       }
