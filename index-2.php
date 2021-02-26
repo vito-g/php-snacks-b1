670b25ac0dac8ef59784711c71b4b81e4aa90c4f
Vito Gentile <gentilevito1076@gmail.com>
2021-02-26 17:56:20 +0100

    Inserimento Variabili & Parametri GET

---
+++ index-2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP-Snaks-B1-Validation</title>
+  </head>
+  <body>
+
+
+    <?php
+      $name = $_GET['name'];
+      $email = $_GET['email'];
+      $age = $_GET['age'];
+      $occurrencyOne = '@';
+      $occurrencyTwo = '.';
+
+    
+      // echo 'prova';
+
+     ?>
+
+
+  </body>
+</html>
