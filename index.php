9e19dee1ace7b990c685ba3ad0d8344f38f86421
Vito Gentile <gentilevito1076@gmail.com>
2021-02-26 13:38:01 +0100

    File Preparato

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP-Snaks-B1</title>
+  </head>
+  <body>
+
+    <?php
+
+
+     ?>
+
+  </body>
+</html>
